26571b0513468935bd3df0782e7c6d50d8fc6086
Masih <hoseinamini1380@gmail.com>
2023-07-08 19:13:36 +0330

    add router.js file

---
+++ js/router.js
@@ -0,0 +1,16 @@
+const router = {
+    "/": {
+      template: "online shop.html",
+      title: "",
+    },
+    "/cart": {
+      template: "cart.html",
+      title: "",
+    },
+    "/product details": {
+      template: "product details.html",
+      title: "",
+    },
+  };
+  export default router;
+  
