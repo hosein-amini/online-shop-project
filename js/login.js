6e537067f488a873aa5cb59b4756ac75609cfb03
Hosein Amini <hoseinamini1380@gmail.com>
2023-09-28 00:59:39 +0330

    clean code and fix bugs

--- js/login.js
+++ js/login.js
@@ -24,72 +24,28 @@ let charactersMessage = $.querySelector(".charactersMessage");
 let charactersCountMessage = $.querySelector(".charactersCountMessage");
 let userCharactersCountMessage = $.querySelector(".userCharactersCountMessage");
 
-passwordWrapper.addEventListener("click", (e) => {
-  if (e.target.nodeName === "I") {
-    eye.classList.toggle("hide");
-    eyeSlash.classList.toggle("hide");
-    password.type === "password"
-      ? (password.type = "type")
-      : (password.type = "password");
-  }
-});
-
-function setCookie(userNameValue, passwordValue, rememberFlag) {
+const setCookie = (userNameValue, passwordValue, rememberFlag) => {
   let nowDate = new Date();
   nowDate.setTime(nowDate.getTime() + 10 * 60 * 1000);
   $.cookie = `userName=${userNameValue};Path=/;expires=${nowDate}`;
   $.cookie = `password=${passwordValue};Path=/;expires=${nowDate}`;
   $.cookie = `remember=${rememberFlag};Path=/;expires=${nowDate}`;
-}
-
-function clearInput() {
+};
+const clearInput = () => {
   userName.value = "";
   password.value = "";
-}
-loginButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (regValidUser.test(userName.value)) {
-    if (regValidPass.test(password.value)) {
-      if (checkBox.checked) {
-        setCookie(userName.value, password.value, true);
-        setTimeout(() => {
-          history.back();
-        }, 1000);
-      } else {
-        setCookie(userName.value, password.value, false);
-        setTimeout(() => {
-          history.back();
-        }, 1000);
-      }
-      clearInput();
-    } else {
-      if (!regPassCapitalLetters.test(password.value)) {
-        capitalLettersMessage.classList.remove("hide");
-      }
-      if (!regPassLowercaseLetters.test(password.value)) {
-        lowercaseLettersMessage.classList.remove("hide");
-      }
-      if (!regPassNumbers.test(password.value)) {
-        numbersMessage.classList.remove("hide");
-      }
-      if (!regPassCharacters.test(password.value)) {
-        charactersMessage.classList.remove("hide");
-      }
-      if (!regPassCharactersCount.test(password.value)) {
-        charactersCountMessage.classList.remove("hide");
-      }
-    }
-  } else {
-    userCharactersCountMessage.classList.remove("hide");
-  }
-});
-
-userName.addEventListener("keyup", () => {
+};
+const getCookie = (input, cookieParam) => {
+  input.value = cookieParam.substring(cookieParam.indexOf("=") + 1);
+};
+const hideUserNameError = (e) => {
+  e.key === "Backspace" && showUserNameError();
   if (regValidUser.test(userName.value)) {
     userCharactersCountMessage.classList.add("hide");
   }
-});
-password.addEventListener("keyup", () => {
+};
+const hidePasswordError = (e) => {
+  e.key === "Backspace" && showPasswordError();
   if (regPassCapitalLetters.test(password.value)) {
     capitalLettersMessage.classList.add("hide");
   }
@@ -105,9 +61,65 @@ password.addEventListener("keyup", () => {
   if (regPassCharactersCount.test(password.value)) {
     charactersCountMessage.classList.add("hide");
   }
-});
-
-userName.addEventListener("focus", () => {
+};
+const showUserNameError = () => {
+  userCharactersCountMessage.classList.remove("hide");
+};
+const showPasswordError = () => {
+  if (!regPassCapitalLetters.test(password.value)) {
+    capitalLettersMessage.classList.remove("hide");
+  }
+  if (!regPassLowercaseLetters.test(password.value)) {
+    lowercaseLettersMessage.classList.remove("hide");
+  }
+  if (!regPassNumbers.test(password.value)) {
+    numbersMessage.classList.remove("hide");
+  }
+  if (!regPassCharacters.test(password.value)) {
+    charactersMessage.classList.remove("hide");
+  }
+  if (!regPassCharactersCount.test(password.value)) {
+    charactersCountMessage.classList.remove("hide");
+  }
+};
+const checkBoxHandler = () => {
+  if (checkBox.checked) {
+    setCookie(userName.value, password.value, true);
+  } else {
+    setCookie(userName.value, password.value, false);
+  }
+};
+const passwordValidator = () => {
+  if (regValidPass.test(password.value)) {
+    checkBoxHandler();
+    setTimeout(() => history.back(), 1000);
+    clearInput();
+  } else {
+    showPasswordError();
+  }
+};
+const userNameValidator = () => {
+  if (regValidUser.test(userName.value)) {
+    return true;
+  } else {
+    showUserNameError();
+    return false;
+  }
+};
+const submitForm = (e) => {
+  e.preventDefault();
+  userNameValidator() && passwordValidator();
+};
+const togglePasswordVisibility = (e) => {
+  if (e.target.nodeName === "I") {
+    eye.classList.toggle("hide");
+    eyeSlash.classList.toggle("hide");
+    password.type === "password"
+      ? (password.type = "type")
+      : (password.type = "password");
+  }
+};
+const pushCookiesToInputs = () => {
   if ($.cookie) {
     let cookieArray = $.cookie.split("; ");
     if (cookieArray[2].substring(cookieArray[2].indexOf("=") + 1) == "true") {
@@ -118,8 +130,10 @@ userName.addEventListener("focus", () => {
       }
     }
   }
-});
+};
 
-function getCookie(input, cookieParam) {
-  input.value = cookieParam.substring(cookieParam.indexOf("=") + 1);
-}
+userName.addEventListener("keyup", hideUserNameError);
+password.addEventListener("keyup", hidePasswordError);
+userName.addEventListener("focus", pushCookiesToInputs);
+passwordWrapper.addEventListener("click", togglePasswordVisibility);
+loginButton.addEventListener("click", submitForm);
